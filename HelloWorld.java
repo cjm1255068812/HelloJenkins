5e09128c47cd22416392aed4bfac262ec8c328a2
junming.chen <junming.chen@weimob.com>
2018-08-23 14:13:30 +0800

    修改HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
-public class Demo{
+public class HelloWorld{
 	
-	public void static main(String[] args){
-		
-	System.out.println("Hello World");
-
-	}
+public static void main(String[] args) {
+    System.out.println("Hello World"); 
+    }
 }
